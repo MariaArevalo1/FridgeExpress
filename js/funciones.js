260a3fe8431b6db67dd8c5a89e9cc8fe2faff562
MariaArevalo1 <45887505+MariaArevalo1@users.noreply.github.com>
2020-04-23 21:02:30 -0700

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,13 @@
+//Función para animar el botón de scroll
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+
+});
---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(function(){	
+	$('#btnInicio').click(function(){
+		$('#registroModal').modal();
+	});	
+});
